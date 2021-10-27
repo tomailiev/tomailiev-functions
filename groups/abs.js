3cbff23f983112abafe181b64c5350a93d7bd702
Toma Iliev <iliev.toma@gmail.com>
2021-10-27 16:06:58 -0700

    fixed minor bugs in fetch

--- groups/abs.js
+++ groups/abs.js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch');
 const apiUri = 'https://api1.tix.com/api/onlinesales/events/organization/2641';
-const tixUri = `${apiUri}/event/`;
+const tixUri = 'https://www.tix.com/ticket-sales/americanbach/2641/event/';
 
 function getAbsEvents() {
-    fetch(apiUri)
+    return fetch(apiUri)
         .then(x => x.json())
-        .then(({ payload: groupedEvents }) => {
-            return groupedEvents.reduce((prev, curr) => {
+        .then(({ payload }) => {
+            return payload.groupedEvents.reduce((prev, curr) => {
                 return prev.concat(curr.map(event => {
                     return {
                         dateTime: event.EventDate,
                         eventName: event.ProductionName,
-                        eventUrl: `${tixUri}${event.EventID}`,
+                        eventUrl: `${tixUri}${event.EventId}`,
                         groupName: 'American Bach Soloists',
                         location: `${event.VenueCity} ${event.VenueState}`,
                         venue: event.VenueName,
--- groups/phil.js
+++ groups/phil.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch');
 const apiUri = 'https://www.cityboxoffice.com/include/widgets/events/performancelist.asp?fromDate=&toDate=&venue=0&city=&swEvent=0&category=33&searchString=&searchType=0&showHidden=0&showPackages=1&action=perf&listPageSize=35&listMaxSize=999&page=1&cp=0';
 const tixUri = 'https://www.cityboxoffice.com/ordertickets.asp?p=';
 function getPhilEvents() {
-    fetch(apiUri)
+    return fetch(apiUri)
         .then(x => x.json())
         .then(({ performance }) => {
             return performance.reduce((prev, curr) => {
                 return prev.concat({
                     dateTime: curr.PerformanceDateTime,
                     eventName: curr.PerformanceName,
-                    eventUrl: `${tixUri}${curr.EventID}`,
+                    eventUrl: `${tixUri}${curr.PerformanceID}`,
                     groupName: curr.Event,
                     location: `${curr.VenueCity} ${curr.VenueState}`,
                     venue: curr.Venue,
